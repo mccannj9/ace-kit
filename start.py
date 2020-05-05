26d5ec61e86b72132c86495f91aa3778668ae90a
mccannj9 <jamie.mccann@gmx.at>
2020-05-05 22:01:02 +0200

    Updated start script to have argument parser

--- start.py
+++ start.py
@@ -1,18 +1,34 @@
 #! /usr/bin/env python3
 
 import os
-import sys
+import argparse
 
 from matplotlib import pyplot
 
 from kit.finder import SwitchpointFinder
 
-try:
-    fn = sys.argv[2]
-except IndexError:
-    fn = "./"
+parser = argparse.ArgumentParser()
 
-finder = SwitchpointFinder(sys.argv[1], fn)
+parser.add_argument('-a', '--acefile', required=True)
+parser.add_argument('-o', '--output-dir', required=False, default="./", type=str)
+parser.add_argument('-w', '--window-size', required=False, default=7, type=int)
+parser.add_argument('-d', '--min-depth', required=False, default=10, type=int)
+parser.add_argument('-r', '--min-read-prop', required=False, default=0.1, type=float)
+args = parser.parse_args()
+
+try:
+    os.mkdir(args.output_dir)
+
+except FileExistsError:
+    pass
+
+keyword_args = {
+    'window_size': args.window_size,
+    'min_depth': args.min_depth,
+    'min_read_prop': args.min_read_prop
+}
+
+finder = SwitchpointFinder(args.acefile, args.output_dir, **keyword_args)
 acefile = finder.acefile
 
 results_dict = finder.fit()
