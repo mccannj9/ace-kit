620a0000e48ab024ca9ef5fb756151dda99ebfa8
mccannj9 <jamie.mccann@gmx.at>
2020-05-26 12:21:43 +0200

    Pair data class and script uncomplements reads before analysis

--- pairs_analysis.py
+++ pairs_analysis.py
@@ -6,6 +6,7 @@ import pickle
 from dataclasses import dataclass
 from difflib import get_close_matches
 
+from kit.contig import Read
 
 @dataclass
 class Pair:
@@ -13,7 +14,7 @@ class Pair:
     r: Read
 
     def __bool__(self):
-        return self.f * self.r
+        return self.f * self.r != 0
 
     def set_reference(self):
         self.reference = self.f if self.f else self.r
@@ -23,6 +24,11 @@ class Pair:
         self.f.kmers = {self.f.seq[x:x+k]:x for x in range(len(self.f)-k+1)}
         self.r.kmers = {self.r.seq[x:x+k]:x for x in range(len(self.r)-k+1)}
 
+    def uncomplement_reads(self):
+        if self.f.comp == 'C':
+            self.f._comp_seq()
+        if self.r.comp == 'C':
+            self.r._comp_seq()
 
 reads_pickle = sys.argv[1]
 min_k = 7
@@ -34,6 +40,7 @@ with open(reads_pickle, 'rb') as pkl:
 pairs_list = []
 for _, r1, r2 in mate_pairs:
     pair = Pair(f=r1, r=r2) if r1.name.endswith('f') else Pair(f=r2, r=r1)
+    pair.uncomplement_reads()
     pairs_list.append(pair)
 
 for pair in pairs_list:
