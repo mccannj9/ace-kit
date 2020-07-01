dc0f5ed45d09d5fa13d136cd5476ad919b91cfa8
Jamie <jamie.mccann@gmx.at>
2020-07-01 16:04:27 +0200

    Fixed relative path problems, now summary table is functional

--- almitey.py
+++ almitey.py
@@ -8,8 +8,9 @@ import argparse
 from kit.finder import SwitchpointFinder
 from kit.blast import set_result_orientation, quick_blastn, parse_blast_output
 from kit.utils import find_all_boundary_reads, pair_boundary_reads, pairs_with_correct_orient
-from kit.html import build_html_output, major_row_template, major_html_template
 
+from kit.html import build_html_output, major_html_template
+from kit.html import major_row_template, major_row_none_template
 
 class Almitey(object):
     def __init__(
@@ -26,7 +27,7 @@ class Almitey(object):
         self.suffices = suffices
         self.relative_loc = "seqclust/clustering/clusters"
 
-    def run(self, cluster):
+    def run(self, cluster, all=False):
 
         cluster_output_dict = {
             'cluster': '',
@@ -37,16 +38,15 @@ class Almitey(object):
             'minor_path': ""
         }
 
+        clname = os.path.basename(cluster).split("_")[-1]
+        cluster_output_dict['cluster'] = clname
+
         try:
             self.ace_filename = glob.glob(f"{cluster}/*.ace")[0]
 
         except IndexError:
             print(f"No ace file found in {self.input_dir}", file=sys.stderr)
-            return major_row_template.safe_substitute(cluster_output_dict)
-
-        # clname = self.output_dir.split("/")[-2].split("_")[-1]
-        clname = os.path.basename(self.output_dir).split("_")[-1]
-        cluster_output_dict['cluster'] = clname
+            return major_row_none_template.safe_substitute(cluster_output_dict)
 
         try:
             os.mkdir(self.output_dir)
@@ -77,13 +77,17 @@ class Almitey(object):
                     boundaries += c.boundaries
                 boundaries.sort(key=lambda x: x.rate, reverse=True)
 
+                if all:
+                    # setting up paths
+                    for b in boundaries:
+                        b.logo_path = os.path.basename(b.logo_path)
+                    dirname = os.path.basename(cluster)
+                    cluster_output_dict['minor_path'] = f"{self.relative_loc}/{dirname}/almitey/almitey.html"
+                
                 with open(f"{self.output_dir}/almitey.html", 'w') as html:
                     clname = boundaries[0].contig.name.split("Contig")[0]
                     html_text = build_html_output(clname, boundaries)
                     print(html_text, file=html)
-
-                minor_path = f"{self.relative_loc}/{os.path.basename(html.name)}"
-                cluster_output_dict['minor_path'] = minor_path
 
         return major_row_template.safe_substitute(cluster_output_dict)
 
@@ -91,6 +95,9 @@ class Almitey(object):
     def run_on_all_clusters(self):
         clusters = glob.glob(
             f"{self.input_dir}/{self.relative_loc}/dir_CL*"
+        )
+        clusters.sort(
+            key=lambda c: int(os.path.basename(c).split("_")[-1][2:])
         )
 
         self.major_html_fn = f"{self.input_dir}/almitey.html"
@@ -100,7 +107,7 @@ class Almitey(object):
             print(cluster)
             self.output_dir = f"{cluster}/almitey"
             self.log_filename = f"{self.output_dir}/almitey_log.txt"
-            html = self.run(cluster)
+            html = self.run(cluster, all=True)
             table_rows.append(html)
 
         table_rows = "\n".join(table_rows)
